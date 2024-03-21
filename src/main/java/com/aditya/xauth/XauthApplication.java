e02b339b8468be1a91a05ebc4bc7a1a7e5a7a7db
adityaag530 <120978306+adityaag530@users.noreply.github.com>
2024-03-21 08:59:23 +0530

    project setup completed

---
+++ src/main/java/com/aditya/xauth/XauthApplication.java
@@ -0,0 +1,13 @@
+package com.aditya.xauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aditya/xauth/XauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aditya.xauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
